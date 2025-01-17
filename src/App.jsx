dc5680fedfb0757e3227f099f2264c240c26e6ef
bett-bett <kipbryan.bett@gmail.com>
2025-01-17 15:15:31 +0300

    vite react app

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,37 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const ButtonText = count >= 7 
+    ? ` \`\`\` <button onClick={() => setCount((count) => count + 1)}>count is ${count}</button> \`\`\` `
+    : `count is ${count}`;
+   
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Normal React starter code</h2>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+          {ButtonText}
+          </button>
+        </div>
+
+        <div>      
+          <a href="https://vite.dev" target="_blank" rel="noreferrer">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+
+          {[...Array(count)].map((_, index) => (
+            <a 
+            key={index} 
+              href="https://react.dev" 
+              target="_blank" 
+              rel="noreferrer"
+              >
+              <img src={reactLogo} className="logo react" alt="React logo" />
+            </a>
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
