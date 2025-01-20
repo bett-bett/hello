ece4c4d15040a642a0eec18357ad721dc79d090f
bett-bett <kipbryan.bett@gmail.com>
2025-01-20 09:55:30 +0300

    design and responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Count from './pages/Count';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const ButtonText = count >= 7 
-    ? ` \`\`\` <button onClick={() => setCount((count) => count + 1)}>count is ${count}</button> \`\`\` `
-    : `count is ${count}`;
-   
+  
   return (
     <>
-      <div>
-        <h2>Normal React starter code</h2>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-          {ButtonText}
-          </button>
+        <div className='main'>
+          <h1 className='main-heading'>
+            Bryan's Website
+          </h1>
+          <div className="body">
+            <Count />
+          </div>
         </div>
-
-        <div>      
-          <a href="https://vite.dev" target="_blank" rel="noreferrer">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-
-          {[...Array(count)].map((_, index) => (
-            <a 
-            key={index} 
-              href="https://react.dev" 
-              target="_blank" 
-              rel="noreferrer"
-              >
-              <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
-          ))}
-        </div>
-      </div>
     </>
   )
 }
---
+++ src/pages/Count.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import reactLogo from '../assets/react.svg'
+import viteLogo from '../assets/vite.svg'
+
+function Count() {
+  const [count, setCount] = useState(0)
+  const [showStarter, setStarter] = useState(true)
+
+  const ButtonText = count >= 7 
+    ? ` \`\`\` <button onClick={() => setCount((count) => count + 1)}>count is ${count}</button> \`\`\` `
+    : `count is ${count}`;
+   
+  return (
+    <>
+      <div className='main-body'>
+        <h2 style={{cursor: "pointer"}} onClick={() => setStarter(showStarter => !showStarter)}>
+          Normal React starter code ↓
+        </h2>
+          {
+            showStarter === false
+              ? <></>
+              :
+            (  
+            <div className='main-starter'>
+
+            <div className="card">
+              <button onClick={() => setCount((count) => count + 1)}>
+              {ButtonText}
+              </button>
+            </div>
+
+            <div>      
+              <a href="https://vite.dev" target="_blank" rel="noreferrer">
+                <img src={viteLogo} className="logo" alt="Vite logo" />
+              </a>
+
+              {[...Array(count)].map((_, index) => (
+                <a 
+                key={index} 
+                href="https://react.dev" 
+                target="_blank" 
+                rel="noreferrer"
+                title="Why is the first react logo the only spinning logo? PR"
+                >
+                  <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+              ))}
+            </div>
+          </div>
+          )
+        } 
+      </div>
+    </>
+  )
+}
+
+export default Count
